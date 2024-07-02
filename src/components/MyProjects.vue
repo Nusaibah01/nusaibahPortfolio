86635f17d7996b3dd31b26f7b53178a3aae74be8
Nusaibah01 <charifnusaibah01@gmail.com>
2024-07-02 12:52:48 +0200

    projectsStyling

--- src/components/MyProjects.vue
+++ src/components/MyProjects.vue
@@ -3,12 +3,15 @@
      <div class="row">
         <h2 class="display-2" id="p">My Projects</h2>
     </div>
-        <div class="row gap-2 justify-content-center" v-if="myProjects">
+        <div class="row gap-2 justify-content-center d-flex align-items-stretch" v-if="myProjects">
           
-            <Card v-for="(project, id) in myProjects" :key="id">
+            <Card v-for="(project, id) in myProjects" :key="id" class="card">
                 <template #cardHeader>
-                    <h3>{{ project.projectName }}</h3>
-                    <img :src="project.image" :alt="project.projectName" class="img-fluid" loading="lazy">
+                    <h3 class="d-inline">{{ project.projectName }}</h3>
+                    <div>
+                    <img src="https://nusaibah01.github.io/all-images/images/Health Potion.gif" alt="Projects.gif">
+                </div>
+                    <!-- <img :src="project.image" :alt="project.projectName" class="img-fluid" loading="lazy"> -->
                 </template>
                 <template #cardBody>
                     <p>{{ project.description }} </p>
@@ -55,5 +58,26 @@ h3{
 p{
     font-family: "Space Mono", monospace;
     color: blue;
+}
+img{
+    padding: 0;
+    width: 48px;
+    height: 48px;
+}
+button{
+    border-radius: 3px;
+    background-color: white;
+    margin-bottom: 30px;
+}
+.container{
+    background-color: black;
+}
+.card{
+    background-color: #0C0C0C;
+    
+}
+h2{
+    margin: 30px;
+    font-family: "Press Start 2P", system-ui !important
 }
 </style>
